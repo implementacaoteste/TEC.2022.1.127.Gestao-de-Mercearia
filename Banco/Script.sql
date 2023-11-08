8cd8dddb754310a4ee313d8e91a79e7b3fbe7169
Keyton Bessa <113356319+KeytonnBessa@users.noreply.github.com>
2023-11-08 09:54:25 -0300

    Banco script atualizado

--- Banco/Script.sql
+++ Banco/Script.sql
@@ -1,18 +1,19 @@
 USE master
 GO
 
-IF(EXISTS(SELECT 1 FROM master.sys.databases WHERE name = 'Mercearia'))
-ALTER DATABASE Mercearia SET  SINGLE_USER WITH ROLLBACK IMMEDIATE
+IF(EXISTS(SELECT 1 FROM master.sys.databases WHERE name = 'GestaoMercearia'))
+ALTER DATABASE GestaoMercearia SET  SINGLE_USER WITH ROLLBACK IMMEDIATE
 GO
 
-IF(EXISTS(SELECT 1 FROM master.sys.databases WHERE name = 'Mercearia'))
-DROP DATABASE Mercearia
+IF(EXISTS(SELECT 1 FROM master.sys.databases WHERE name = 'GestaoMercearia'))
+DROP DATABASE GestaoMercearia
 GO
 
-CREATE DATABASE Mercearia
+IF(NOT EXISTS(SELECT 1 FROM master.sys.databases WHERE name = 'GestaoMercearia'))
+CREATE DATABASE GestaoMercearia
 GO
 
-USE Mercearia
+USE GestaoMercearia
 GO
 
 IF OBJECT_ID('Usuario', 'U') IS NULL
@@ -62,35 +63,111 @@ CREATE TABLE PermissaoGrupoUsuario
 )
 GO
 
-CREATE TABLE Cliente
+
+CREATE TABLE [dbo].[Cliente](
+	[Id] [int] NOT NULL,
+	[Nome] [varchar](150) NULL,
+	[Endereço] [varchar](150) NULL,
+	[Telefone] [varchar](15) NULL,
+ CONSTRAINT [PK_Cliente] PRIMARY KEY CLUSTERED 
 (
-	Id INT PRIMARY KEY IDENTITY(1,1),
-	Nome VARCHAR(150),
-	CPF VARCHAR(15),
-	RG VARCHAR(15),
-	Email VARCHAR(200),
-	Fone VARCHAR(15)
-)
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
 GO
 
-CREATE TABLE Fornecedor
+CREATE TABLE [dbo].[Estoque](
+	[Id] [int] NOT NULL,
+	[DataDeEntrada] [date] NULL,
+	[DataDeSaida] [date] NULL,
+ CONSTRAINT [PK_Estoque] PRIMARY KEY CLUSTERED 
 (
-	Id INT PRIMARY KEY IDENTITY(1,1),
-	Nome VARCHAR(150),
-	CPF VARCHAR(15),
-	RG VARCHAR(15),
-	Email VARCHAR(200),
-	Fone VARCHAR(15)
-)
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
 GO
 
-CREATE TABLE Produto
+CREATE TABLE [dbo].[Funcionario](
+	[Id] [int] NOT NULL,
+	[Nome] [varchar](150) NULL,
+	[Telefone] [varchar](15) NULL,
+	[Login] [varchar](150) NULL,
+	[Senha] [varchar](50) NULL,
+	[Ativo] [bit] NULL,
+	[Salario] [float] NULL,
+ CONSTRAINT [PK_Funcionario] PRIMARY KEY CLUSTERED 
 (
-	Id INT PRIMARY KEY IDENTITY(1,1),
-	Nome VARCHAR(150),
-	Descricao VARCHAR(250),
-	Valor FLOAT	
-)
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
+CREATE TABLE [dbo].[Produto](
+	[Id] [int] NOT NULL,
+	[Nome] [varchar](150) NULL,
+	[Descrição] [varchar](50) NULL,
+	[Categoria] [varchar](50) NULL,
+	[Preço] [float] NULL,
+	[QuantidadeEmEstoque] [int] NULL,
+ CONSTRAINT [PK_Produto] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+CREATE TABLE [dbo].[Senha](
+	[Id] [int] NOT NULL,
+	[IdCliente] [int] NOT NULL,
+ CONSTRAINT [PK_Senha] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Senha]  WITH CHECK ADD  CONSTRAINT [FK_Senha_Cliente] FOREIGN KEY([IdCliente])
+REFERENCES [dbo].[Cliente] ([Id])
+GO
+
+ALTER TABLE [dbo].[Senha] CHECK CONSTRAINT [FK_Senha_Cliente]
+GO
+
+CREATE TABLE [dbo].[Venda](
+	[Id] [int] NOT NULL,
+	[IdFuncionario] [int] NOT NULL,
+	[IdCliente] [int] NOT NULL,
+	[IdProduto] [int] NOT NULL,
+	[PreçoVenda] [float] NULL,
+	[IdFormaPagamento] [int] NULL,
+	[DataVenda] [date] NULL,
+ CONSTRAINT [PK_Venda] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Venda]  WITH CHECK ADD  CONSTRAINT [FK_Venda_Cliente] FOREIGN KEY([IdCliente])
+REFERENCES [dbo].[Cliente] ([Id])
+GO
+
+ALTER TABLE [dbo].[Venda] CHECK CONSTRAINT [FK_Venda_Cliente]
+GO
+
+ALTER TABLE [dbo].[Venda]  WITH CHECK ADD  CONSTRAINT [FK_Venda_Funcionario] FOREIGN KEY([IdFuncionario])
+REFERENCES [dbo].[Funcionario] ([Id])
+GO
+
+ALTER TABLE [dbo].[Venda] CHECK CONSTRAINT [FK_Venda_Funcionario]
+GO
+
+ALTER TABLE [dbo].[Venda]  WITH CHECK ADD  CONSTRAINT [FK_Venda_Produto] FOREIGN KEY([IdProduto])
+REFERENCES [dbo].[Produto] ([Id])
+GO
+
+ALTER TABLE [dbo].[Venda] CHECK CONSTRAINT [FK_Venda_Produto]
 GO
 
 
