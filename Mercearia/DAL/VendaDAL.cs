f0e64fddf54fbfc838cb21a87fe2fa0bc846a6ef
Hiago Cruz <148218580+HiagoMaster@users.noreply.github.com>
2023-11-10 10:03:06 -0300

    Implementação do método Alterar na VendaDAL

--- Mercearia/DAL/VendaDAL.cs
+++ Mercearia/DAL/VendaDAL.cs
@@ -42,5 +42,37 @@ namespace DAL
                 cn.Close();
             }
         }
+        public void Alterar(Venda _venda)
+        {
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = cn.CreateCommand();
+                cmd.CommandText = @"UPDATE Venda SET IdFuncionario = @IdFuncionario, IdCliente = @IdCliente, IdProduto = @IdProduto, PrecoVenda = @PrecoVenda, IdFormaPagamento = @IdFormaPagamento, DataVenda = @DataVenda WHERE Id = @Id";
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                cmd.Parameters.AddWithValue("@Id", _venda.Id);
+                cmd.Parameters.AddWithValue("@IdFuncionario", _venda.IdFuncionario);
+                cmd.Parameters.AddWithValue("@IdCliente", _venda.IdCliente);
+                cmd.Parameters.AddWithValue("@IdProduto", _venda.IdProduto);
+                cmd.Parameters.AddWithValue("@PrecoVenda", _venda.PrecoVenda);
+                cmd.Parameters.AddWithValue("@IdFormaPagamento", _venda.IdFormaPagamento);
+                cmd.Parameters.AddWithValue("@DataVenda", _venda.DataVenda);
+
+                cmd.Connection = cn;
+                cn.Open();
+
+                cmd.ExecuteNonQuery();
+
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar alterar uma venda no banco de dados.", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
     }
 }
