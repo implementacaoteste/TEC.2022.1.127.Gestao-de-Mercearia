4b4b8f0beea8512b4c188e44c94b6fea1c73a5d5
Ana Beatriz Gomes da Conceição <148218909+BeatriXVIII@users.noreply.github.com>
2023-11-10 11:29:50 -0300

    Modificação na interface

--- Mercearia/UIGestaoMercearia/Properties/Resources.Designer.cs
+++ Mercearia/UIGestaoMercearia/Properties/Resources.Designer.cs
@@ -99,5 +99,25 @@ namespace UIGestaoMercearia.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap shopping_cart_icon_illustration_free_vector__2_1 {
+            get {
+                object obj = ResourceManager.GetObject("shopping-cart-icon-illustration-free-vector (2)1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap shopping_cart_icon_illustration_free_vector1 {
+            get {
+                object obj = ResourceManager.GetObject("shopping-cart-icon-illustration-free-vector1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
