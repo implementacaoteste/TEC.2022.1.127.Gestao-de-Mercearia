56f5b3cb763f6eeba8dd405caa6e0819f2181216
Keyton Bessa <113356319+KeytonnBessa@users.noreply.github.com>
2023-11-10 08:35:47 -0300

    Implementacao do método excluir e buscarTodos na FuncionarioDAL

--- Mercearia/DAL/FuncionarioDAL.cs
+++ Mercearia/DAL/FuncionarioDAL.cs
@@ -41,6 +41,114 @@ namespace DAL
             }
         }
 
+        public void Alterar(Funcionario _funcionario)
+        {
+
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = cn.CreateCommand();
+                cmd.CommandText = @"UPDATE Funcionario SET 
+                                        Nome = @Nome, 
+                                        Fone = @Fone, 
+                                        Ativo = @Ativo, 
+                                        Profissao = @Profissao, 
+                                        Salario = @Salario,
+                                    WHERE Id = @Id";
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                cmd.Parameters.AddWithValue("@Id", _funcionario.Id);
+                cmd.Parameters.AddWithValue("@Nome", _funcionario.Nome);
+                cmd.Parameters.AddWithValue("@Fone", _funcionario.Fone);
+                cmd.Parameters.AddWithValue("@Ativo", _funcionario.Ativo);
+                cmd.Parameters.AddWithValue("@Profissao", _funcionario.Profissao);
+                cmd.Parameters.AddWithValue("@Salario", _funcionario.Salario);
+
+
+                cmd.Connection = cn;
+                cn.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro ao tentar alterar funcionario no banco de dados", ex) { Data = { { "Id", 1004 } } };
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+        public void Excluir(int _id)
+        {
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = cn.CreateCommand();
+                cmd.CommandText = @"DELETE FROM Funcionario WHERE id = @id";
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                cmd.Parameters.AddWithValue("@Id", _id);
+
+                cmd.Connection = cn;
+                cn.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar excluir Funcionario no banco de dados.", ex) { Data = { { "Id", 1005 } } };
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+        public List<Funcionario> BuscarTodos()
+        {
+            List<Funcionario> funcionarios = new List<Funcionario>();
+            Funcionario funcionario = new Funcionario();
+
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandText = "SELECT Id, Nome, Fone, Ativo, Profissao, Salario FROM Funcionario";
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                cn.Open();
+
+                using (SqlDataReader rd = cmd.ExecuteReader())
+                {
+                    while (rd.Read())
+                    {
+                        funcionario = new Funcionario();
+                        funcionario.Id = Convert.ToInt32(rd["Id"]);
+                        funcionario.Nome = rd["Nome"].ToString();
+                        funcionario.Fone = rd["Fone"].ToString();
+                        funcionario.Ativo = Convert.ToBoolean(rd["Ativo"]);
+                        funcionario.Profissao = rd["CodigoDeBarra"].ToString();
+                        funcionario.Salario = (double)rd["Marca"];
+
+                        funcionarios.Add(funcionario);
+
+                    }
+                    return funcionarios;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar buscar todos os funcionarios no banco de dados", ex);
+
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
 
 
     }
