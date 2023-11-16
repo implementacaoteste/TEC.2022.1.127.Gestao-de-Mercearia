040b398beef15b946dd3f70202f6be56e39add03
Ana Beatriz Gomes da Conceição <148218909+BeatriXVIII@users.noreply.github.com>
2023-11-16 11:16:39 -0300

    implementar o método Alterar na EstoqueDAL

--- Mercearia/DAL/EstoqueDAL.cs
+++ Mercearia/DAL/EstoqueDAL.cs
@@ -42,5 +42,37 @@ namespace DAL
                 cn.Close();
             }
         }
+        public void Alterar(Estoque _estoque)
+        {
+
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = cn.CreateCommand();
+                cmd.CommandText = @"UPDATE Estoque SET (Id = @Id , DatadeEntrada = @DatadeEntrada, DatadeSaida = @DatadeSaida, EstoqueMinimo = @EstoqueMinimo, EstoqueMaximo = @EstoqueMaximo, IdProduto = @IdProduto WHERE Id = @Id)"; 
+                                    
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                cmd.Parameters.AddWithValue("@Id", _estoque.Id);
+                cmd.Parameters.AddWithValue("@DatadeEntrada", _estoque.DatadeEntrada);
+                cmd.Parameters.AddWithValue("@DatadeSaida", _estoque.DatadeSaida);
+                cmd.Parameters.AddWithValue("@EstoqueMinimo", _estoque.EstoqueMinimo);
+                cmd.Parameters.AddWithValue("@EstoqueMaximo", _estoque.EstoqueMaximo);
+                cmd.Parameters.AddWithValue("@IdProduto", _estoque.IdProduto);
+
+                cmd.Connection = cn;
+                cn.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro ao tentar alterar estoque no banco de dados", ex) { Data = { { "Id", 20 } } };
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
     }
 }
