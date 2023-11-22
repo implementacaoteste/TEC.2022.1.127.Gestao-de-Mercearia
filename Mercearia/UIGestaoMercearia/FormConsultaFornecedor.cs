ed57cb82af1888f5b7229f4001700319f6245722
Keyton Bessa <113356319+KeytonnBessa@users.noreply.github.com>
2023-11-22 08:46:00 -0300

    Método Buscar na FormConsultaFornecedor implementado

--- Mercearia/UIGestaoMercearia/FormConsultaFornecedor.cs
+++ Mercearia/UIGestaoMercearia/FormConsultaFornecedor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BLL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,7 +20,26 @@ namespace UIGestaoMercearia
 
         private void buttonBuscar_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                switch (comboBoxBuscarPor.SelectedIndex)
+                {
+                    case 0:
+                        if (String.IsNullOrEmpty(textBoxBuscarPor.Text))
+                            throw new Exception("Informe um Id para fazer a busca.") { Data = { { "Id", 10031 } } };
+                            bindingSourceFornecedor.DataSource = new FornecedorBLL().BuscarPorNome(textBoxBuscarPor.Text);
+                        break;
+                    case 1:
+                        bindingSourceFornecedor.DataSource = new FornecedorBLL().BuscarTodos();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void buttonAlterar_Click(object sender, EventArgs e)
