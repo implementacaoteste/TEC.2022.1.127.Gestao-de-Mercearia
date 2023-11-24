84ca53462ec005bcb8ea399d85c7e2b78cc3b3cb
Keyton Bessa <113356319+KeytonnBessa@users.noreply.github.com>
2023-11-24 11:26:38 -0300

    Marca e Categoria criadas na Models, MarcaDAL feita e implementada método de inserção

---
+++ Mercearia/DAL/MarcaDAL.cs
@@ -0,0 +1,39 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class MarcaDAL
+    {
+        public void Inserir(Marca _marca)
+        {
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = cn.CreateCommand();
+                cmd.CommandText = @"INSERT INTO Marca(Marca) 
+                                    VALUES(@Marca)";
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                cmd.Parameters.AddWithValue("@Marca", _marca.Nome);
+                cmd.Connection = cn;
+                cn.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu erro ao tentar inserir uma marca no banco de dados.", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+    }
+}
