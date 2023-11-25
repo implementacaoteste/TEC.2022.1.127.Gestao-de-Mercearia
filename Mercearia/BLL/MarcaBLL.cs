c35504ee1ab6fe837a8ab2a69a4b33a7b97be862
Keyton Bessa <113356319+KeytonnBessa@users.noreply.github.com>
2023-11-24 21:49:33 -0300

    Implementado métodos na MarcaBLL

--- Mercearia/BLL/MarcaBLL.cs
+++ Mercearia/BLL/MarcaBLL.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DAL;
+using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,25 @@ namespace BLL
 {
     public class MarcaBLL
     {
-
+        public void Inserir(Marca _marca)
+        {
+            new MarcaDAL().Inserir(_marca);
+        }
+        public void Alterar(Marca _marca)
+        {
+            new MarcaDAL().Alterar(_marca);
+        }
+        public void Excluir(int _id)
+        {
+            new MarcaDAL().Excluir(_id);
+        }
+        public List<Marca> BuscarTodos(Marca _marca)
+        {
+            return new MarcaDAL().BuscarTudo();
+        }
+        public Marca BuscarPorId(int _id)
+        {
+            return new MarcaDAL().BuscarPorId(_id);
+        }
     }
 }
