f607d84832d326e27224e1c4634d85dbdd9df7a7
Hiago Cruz <148218580+HiagoMaster@users.noreply.github.com>
2023-11-28 10:49:05 -0300

    Implementação do metodo Buscar_Click no FormConsultaFuncionario

--- Mercearia/UIGestaoMercearia/FormConsultaFuncionario.cs
+++ Mercearia/UIGestaoMercearia/FormConsultaFuncionario.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BLL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,10 +20,37 @@ namespace UIGestaoMercearia
 
         private void buttonBuscar_Click(object sender, EventArgs e)
         {
-            
-            
-                
-            
+            try
+            {
+                switch (comboBoxBuscarPor.SelectedIndex)
+                {
+                    case 0:
+                        if (String.IsNullOrEmpty(textBoxBuscar.Text))
+                            throw new Exception("Informe um Id para fazer a busca.") { Data = { { "Id", 10130 } } };
+                        funcionarioBindingSource.DataSource = new FuncionarioBLL().BuscarPorNome(textBoxBuscar.Text);
+                        break;
+                    case 1:
+                        funcionarioBindingSource.DataSource = new FuncionarioBLL().BuscarTodos();
+                        break;
+                    default:
+                        break;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
+
     }
 }
+
+
+
+
+
+
+        
+    
+
