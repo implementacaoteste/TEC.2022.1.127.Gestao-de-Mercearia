1044753d0070563864ff001946afab3f572b3474
sabrynnasousa09 <148219059+sabrynnasousa09@users.noreply.github.com>
2023-11-30 08:43:05 -0300

    Removida a referência para o evento que não existe mais

--- Mercearia/UIGestaoMercearia/FormCadastroFuncionario.Designer.cs
+++ Mercearia/UIGestaoMercearia/FormCadastroFuncionario.Designer.cs
@@ -146,7 +146,6 @@
             this.button1.TabIndex = 27;
             this.button1.Text = "Salvar";
             this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
