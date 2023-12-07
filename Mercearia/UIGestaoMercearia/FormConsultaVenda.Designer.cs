a4abb4e1b30cdc8dbedf20bd84c385a979658571
Ana Beatriz Gomes da Conceição <148218909+BeatriXVIII@users.noreply.github.com>
2023-12-07 09:10:25 -0300

    Resolvendo conflitos

--- Mercearia/UIGestaoMercearia/FormConsultaVenda.Designer.cs
+++ Mercearia/UIGestaoMercearia/FormConsultaVenda.Designer.cs
@@ -182,7 +182,6 @@
             this.label1.Size = new System.Drawing.Size(409, 44);
             this.label1.TabIndex = 0;
             this.label1.Text = "CONSULTA DE VENDAS";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // panel2
             // 
