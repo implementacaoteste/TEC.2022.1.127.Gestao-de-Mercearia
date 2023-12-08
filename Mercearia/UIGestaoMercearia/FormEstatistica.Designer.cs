06056220bc90aa8ff790d3724505f0769fafadf4
Hiago Cruz <148218580+HiagoMaster@users.noreply.github.com>
2023-12-08 09:26:36 -0300

    Update

--- Mercearia/UIGestaoMercearia/FormEstatistica.Designer.cs
+++ Mercearia/UIGestaoMercearia/FormEstatistica.Designer.cs
@@ -28,17 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // FormEstatistica
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "FormEstatistica";
             this.Text = "FormEstatistica";
-            this.ResumeLayout(false);
-
         }
 
         #endregion
