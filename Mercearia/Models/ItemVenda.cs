014a5c18b2638a673f140de020a55041e49e1456
Keyton Bessa <113356319+KeytonnBessa@users.noreply.github.com>
2024-01-25 09:11:11 -0300

    Criei uma tela PDV para testes

---
+++ Mercearia/Models/ItemVenda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class ItemVenda
+    {
+        public int Id { get; set; }
+        public int IdProduto { get; set; }
+        public int IdVenda { get; set; }
+        public int Quantidade { get; set; }
+        public double ValorUnitario { get; set; }
+        public double SubTotal { get; set; }
+        public Produto Produto { get; set; }
+    }
+}
