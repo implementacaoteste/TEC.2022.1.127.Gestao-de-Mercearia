b40da10d608055c6d9bc6aa002abd0efbaf4bded
Keyton Bessa <113356319+KeytonnBessa@users.noreply.github.com>
2024-02-01 10:07:58 -0300

    FormCadastroPagamento Criado

--- Mercearia/UIGestaoMercearia/FormCadastroPagamento.Designer.cs
+++ Mercearia/UIGestaoMercearia/FormCadastroPagamento.Designer.cs
@@ -28,12 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormCadastroPagamento";
+            label1 = new Label();
+            textBox1 = new TextBox();
+            buttonCancelar = new Button();
+            buttonSalvar = new Button();
+            label10 = new Label();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            checkBox1 = new CheckBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Swis721 Blk BT", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(29, 92);
+            label1.Name = "label1";
+            label1.Size = new Size(64, 24);
+            label1.TabIndex = 59;
+            label1.Text = "Tipo:";
+            // 
+            // textBox1
+            // 
+            textBox1.Cursor = Cursors.IBeam;
+            textBox1.Location = new Point(29, 119);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(401, 27);
+            textBox1.TabIndex = 58;
+            // 
+            // buttonCancelar
+            // 
+            buttonCancelar.BackColor = Color.White;
+            buttonCancelar.FlatStyle = FlatStyle.Flat;
+            buttonCancelar.ForeColor = SystemColors.ActiveCaptionText;
+            buttonCancelar.Location = new Point(504, 280);
+            buttonCancelar.Name = "buttonCancelar";
+            buttonCancelar.Size = new Size(94, 29);
+            buttonCancelar.TabIndex = 57;
+            buttonCancelar.Text = "&Cancelar";
+            buttonCancelar.UseVisualStyleBackColor = false;
+            // 
+            // buttonSalvar
+            // 
+            buttonSalvar.BackColor = Color.Azure;
+            buttonSalvar.FlatStyle = FlatStyle.Flat;
+            buttonSalvar.Location = new Point(404, 280);
+            buttonSalvar.Name = "buttonSalvar";
+            buttonSalvar.Size = new Size(94, 29);
+            buttonSalvar.TabIndex = 56;
+            buttonSalvar.Text = "&Salvar";
+            buttonSalvar.UseVisualStyleBackColor = false;
+            // 
+            // label10
+            // 
+            label10.BackColor = Color.FromArgb(50, 153, 204);
+            label10.BorderStyle = BorderStyle.Fixed3D;
+            label10.Dock = DockStyle.Top;
+            label10.Font = new Font("Britannic Bold", 22.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label10.ForeColor = SystemColors.ButtonFace;
+            label10.Location = new Point(0, 0);
+            label10.Name = "label10";
+            label10.Size = new Size(610, 61);
+            label10.TabIndex = 55;
+            label10.Text = "CADASTO DE FORMA DE PAGAMENTO";
+            label10.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(29, 194);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(358, 112);
+            textBox2.TabIndex = 60;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Swis721 Blk BT", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = SystemColors.ButtonHighlight;
+            label2.Location = new Point(29, 167);
+            label2.Name = "label2";
+            label2.Size = new Size(120, 24);
+            label2.TabIndex = 59;
+            label2.Text = "Descrição";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.FlatStyle = FlatStyle.Flat;
+            checkBox1.Font = new Font("Swis721 Blk BT", 10.2F, FontStyle.Italic, GraphicsUnit.Point);
+            checkBox1.ForeColor = Color.White;
+            checkBox1.Location = new Point(459, 122);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(139, 24);
+            checkBox1.TabIndex = 61;
+            checkBox1.Text = "Aceita Troco";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // FormCadastroPagamento
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(50, 153, 204);
+            CancelButton = buttonCancelar;
+            ClientSize = new Size(610, 321);
+            Controls.Add(checkBox1);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(buttonCancelar);
+            Controls.Add(buttonSalvar);
+            Controls.Add(label10);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "FormCadastroPagamento";
+            ShowIcon = false;
+            ShowInTaskbar = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button buttonCancelar;
+        private Button buttonSalvar;
+        private Label label10;
+        private TextBox textBox2;
+        private Label label2;
+        private CheckBox checkBox1;
     }
 }
