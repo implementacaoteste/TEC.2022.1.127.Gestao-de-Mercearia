14ce12c0bf8f9b496c1b225f3c4d6c41dedd6a79
Keyton Bessa <113356319+KeytonnBessa@users.noreply.github.com>
2024-02-01 08:15:30 -0300

    ItemVendaBLL criado coms os devidos métodos

---
+++ Mercearia/BLL/ItemVendaBLL.cs
@@ -0,0 +1,32 @@
+﻿using DAL;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class ItemVendaBLL
+    {
+        public void Inserir(ItemVenda _item)
+        {
+            new ItemVendaDAL().Inserir(_item);
+        }
+        public void Alterar(ItemVenda _item)
+        {
+            new ItemVendaDAL().Alterar(_item);
+        }
+        public void Excluir(int _id)
+        {
+            new ItemVendaDAL().Excluir(_id);
+        }
+        public ItemVenda BuscarPorIdVenda(int _idVenda)
+        {
+            return new ItemVendaDAL().BuscarPorIdVenda(_idVenda);
+        }
+
+
+    }
+}
