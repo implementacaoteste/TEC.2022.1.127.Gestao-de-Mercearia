854f856f69cc716f39bfda2173c7a0860d39163a
Hiago Cruz <hiagocruz825@gmail.com>
2024-02-06 08:13:00 -0300

    Update FormCadastroCategoria

--- Mercearia/UIGestaoMercearia/FormCadastroCategoria.Designer.cs
+++ Mercearia/UIGestaoMercearia/FormCadastroCategoria.Designer.cs
@@ -28,103 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.buttonCancelar = new System.Windows.Forms.Button();
-            this.buttonSalvar = new System.Windows.Forms.Button();
-            this.label10 = new System.Windows.Forms.Label();
-            this.textBox1Nome = new System.Windows.Forms.TextBox();
-            this.categoriaBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.categoriaBindingSource)).BeginInit();
-            this.SuspendLayout();
+            components = new System.ComponentModel.Container();
+            buttonCancelar = new Button();
+            buttonSalvar = new Button();
+            label10 = new Label();
+            textBox1Nome = new TextBox();
+            categoriaBindingSource = new BindingSource(components);
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)categoriaBindingSource).BeginInit();
+            SuspendLayout();
             // 
             // buttonCancelar
             // 
-            this.buttonCancelar.BackColor = System.Drawing.Color.White;
-            this.buttonCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonCancelar.ForeColor = System.Drawing.Color.Black;
-            this.buttonCancelar.Location = new System.Drawing.Point(683, 159);
-            this.buttonCancelar.Name = "buttonCancelar";
-            this.buttonCancelar.Size = new System.Drawing.Size(94, 29);
-            this.buttonCancelar.TabIndex = 47;
-            this.buttonCancelar.Text = "&Cancelar";
-            this.buttonCancelar.UseVisualStyleBackColor = false;
-            this.buttonCancelar.Click += new System.EventHandler(this.buttonCancelar_Click);
+            buttonCancelar.BackColor = Color.White;
+            buttonCancelar.FlatStyle = FlatStyle.Flat;
+            buttonCancelar.ForeColor = Color.Black;
+            buttonCancelar.Location = new Point(683, 159);
+            buttonCancelar.Name = "buttonCancelar";
+            buttonCancelar.Size = new Size(94, 29);
+            buttonCancelar.TabIndex = 47;
+            buttonCancelar.Text = "&Cancelar";
+            buttonCancelar.UseVisualStyleBackColor = false;
+            buttonCancelar.Click += buttonCancelar_Click;
             // 
             // buttonSalvar
             // 
-            this.buttonSalvar.BackColor = System.Drawing.Color.Azure;
-            this.buttonSalvar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonSalvar.Location = new System.Drawing.Point(583, 159);
-            this.buttonSalvar.Name = "buttonSalvar";
-            this.buttonSalvar.Size = new System.Drawing.Size(94, 29);
-            this.buttonSalvar.TabIndex = 46;
-            this.buttonSalvar.Text = "Salvar";
-            this.buttonSalvar.UseVisualStyleBackColor = false;
-            this.buttonSalvar.Click += new System.EventHandler(this.buttonSalvar_Click);
+            buttonSalvar.BackColor = Color.Azure;
+            buttonSalvar.FlatStyle = FlatStyle.Flat;
+            buttonSalvar.Location = new Point(583, 159);
+            buttonSalvar.Name = "buttonSalvar";
+            buttonSalvar.Size = new Size(94, 29);
+            buttonSalvar.TabIndex = 46;
+            buttonSalvar.Text = "&Salvar";
+            buttonSalvar.UseVisualStyleBackColor = false;
+            buttonSalvar.Click += buttonSalvar_Click;
             // 
             // label10
             // 
-            this.label10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(153)))), ((int)(((byte)(204)))));
-            this.label10.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label10.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label10.Font = new System.Drawing.Font("Britannic Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label10.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label10.Location = new System.Drawing.Point(0, 0);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(782, 61);
-            this.label10.TabIndex = 31;
-            this.label10.Text = "CADASTRO DE CATEGORIA";
-            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            label10.BackColor = Color.FromArgb(50, 153, 204);
+            label10.BorderStyle = BorderStyle.Fixed3D;
+            label10.Dock = DockStyle.Top;
+            label10.Font = new Font("Britannic Bold", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            label10.ForeColor = SystemColors.ButtonFace;
+            label10.Location = new Point(0, 0);
+            label10.Name = "label10";
+            label10.Size = new Size(782, 61);
+            label10.TabIndex = 31;
+            label10.Text = "CADASTRO DE CATEGORIA";
+            label10.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // textBox1Nome
             // 
-            this.textBox1Nome.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.textBox1Nome.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.categoriaBindingSource, "Nome", true));
-            this.textBox1Nome.Location = new System.Drawing.Point(26, 110);
-            this.textBox1Nome.Name = "textBox1Nome";
-            this.textBox1Nome.Size = new System.Drawing.Size(728, 27);
-            this.textBox1Nome.TabIndex = 48;
+            textBox1Nome.Cursor = Cursors.IBeam;
+            textBox1Nome.DataBindings.Add(new Binding("Text", categoriaBindingSource, "Nome", true));
+            textBox1Nome.Location = new Point(26, 110);
+            textBox1Nome.Name = "textBox1Nome";
+            textBox1Nome.Size = new Size(728, 27);
+            textBox1Nome.TabIndex = 48;
             // 
             // categoriaBindingSource
             // 
-            this.categoriaBindingSource.DataSource = typeof(Models.Categoria);
+            categoriaBindingSource.DataSource = typeof(Models.Categoria);
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Swis721 Blk BT", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(26, 89);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(207, 18);
-            this.label1.TabIndex = 49;
-            this.label1.Text = "INSIRA UMA CATEGORIA";
+            label1.AutoSize = true;
+            label1.Font = new Font("Swis721 Blk BT", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(26, 89);
+            label1.Name = "label1";
+            label1.Size = new Size(207, 18);
+            label1.TabIndex = 49;
+            label1.Text = "INSIRA UMA CATEGORIA";
             // 
             // FormCadastroCategoria
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(153)))), ((int)(((byte)(204)))));
-            this.CancelButton = this.buttonCancelar;
-            this.ClientSize = new System.Drawing.Size(782, 203);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1Nome);
-            this.Controls.Add(this.buttonCancelar);
-            this.Controls.Add(this.buttonSalvar);
-            this.Controls.Add(this.label10);
-            this.KeyPreview = true;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "FormCadastroCategoria";
-            this.ShowIcon = false;
-            this.ShowInTaskbar = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Load += new System.EventHandler(this.FormCadastroCategoria_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.categoriaBindingSource)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(50, 153, 204);
+            CancelButton = buttonCancelar;
+            ClientSize = new Size(782, 203);
+            Controls.Add(label1);
+            Controls.Add(textBox1Nome);
+            Controls.Add(buttonCancelar);
+            Controls.Add(buttonSalvar);
+            Controls.Add(label10);
+            KeyPreview = true;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "FormCadastroCategoria";
+            ShowIcon = false;
+            ShowInTaskbar = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            Load += FormCadastroCategoria_Load;
+            ((System.ComponentModel.ISupportInitialize)categoriaBindingSource).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
