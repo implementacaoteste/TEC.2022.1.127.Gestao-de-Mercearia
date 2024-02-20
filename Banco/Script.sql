81a2ad96785814424426291b70dedc2f622b3d82
Keyton Bessa <113356319+KeytonnBessa@users.noreply.github.com>
2024-02-20 08:32:32 -0300

    banco atualizado 20 02

--- Banco/Script.sql
+++ Banco/Script.sql
@@ -999,6 +999,342 @@ USE [master]
 GO
 ALTER DATABASE [GestaoMercearia] SET  READ_WRITE 
 GO
+USE master
+GO
+
+IF(EXISTS(SELECT 1 FROM master.sys.databases WHERE name = 'GestaoMercearia'))
+ALTER DATABASE GestaoMercearia SET  SINGLE_USER WITH ROLLBACK IMMEDIATE
+GO
+
+IF(EXISTS(SELECT 1 FROM master.sys.databases WHERE name = 'GestaoMercearia'))
+DROP DATABASE GestaoMercearia
+GO
+
+IF(NOT EXISTS(SELECT 1 FROM master.sys.databases WHERE name = 'GestaoMercearia'))
+CREATE DATABASE GestaoMercearia
+GO
+
+USE GestaoMercearia
+GO
+CREATE TABLE [dbo].[Categoria](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Nome] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_Categoria] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Cliente]    Script Date: 20/02/2024 08:29:42 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Cliente](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Nome] [varchar](150) NULL,
+	[Cpf] [varchar](20) NULL,
+	[Endereco] [varchar](150) NULL,
+	[CEP] [varchar](25) NULL,
+	[Email] [varchar](150) NULL,
+	[Telefone] [varchar](15) NULL,
+ CONSTRAINT [PK_Cliente] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Estoque]    Script Date: 20/02/2024 08:29:42 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Estoque](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[DataDeEntrada] [date] NULL,
+	[DataDeSaida] [date] NULL,
+	[EstoqueMinimo] [int] NULL,
+	[EstoqueMaximo] [int] NULL,
+	[QuantidadeEmEstoque] [int] NULL,
+	[IdProduto] [int] NULL,
+ CONSTRAINT [PK_Estoque] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[FormaPagamento]    Script Date: 20/02/2024 08:29:42 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[FormaPagamento](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Tipo] [varchar](100) NOT NULL,
+	[Descricao] [varchar](100) NULL,
+	[Troco] [bit] NOT NULL,
+ CONSTRAINT [PK_FormaPagamento] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Fornecedor]    Script Date: 20/02/2024 08:29:42 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Fornecedor](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Nome] [varchar](150) NULL,
+	[Email] [varchar](150) NULL,
+	[Telefone] [varchar](50) NULL,
+	[Endereco] [varchar](50) NULL,
+	[CEP] [varchar](25) NULL,
+ CONSTRAINT [PK_Fornecedor] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Funcionario]    Script Date: 20/02/2024 08:29:42 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Funcionario](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Nome] [varchar](150) NULL,
+	[Telefone] [varchar](15) NULL,
+	[Ativo] [bit] NULL,
+	[Profissao] [varchar](150) NULL,
+	[Salario] [float] NULL,
+ CONSTRAINT [PK_Funcionario] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[GrupoUsuario]    Script Date: 20/02/2024 08:29:42 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[GrupoUsuario](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[NomeGrupo] [varchar](150) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[ItemVenda]    Script Date: 20/02/2024 08:29:42 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ItemVenda](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[IdProduto] [int] NOT NULL,
+	[IdVenda] [int] NOT NULL,
+	[Quantidade] [int] NOT NULL,
+	[ValorUnitario] [float] NOT NULL,
+	[Subtotal] [float] NULL,
+ CONSTRAINT [PK_ItemVenda_1] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Marca]    Script Date: 20/02/2024 08:29:42 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Marca](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Nome] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_Marca] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Permissao]    Script Date: 20/02/2024 08:29:42 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Permissao](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Descricao] [varchar](250) NULL,
+ CONSTRAINT [PK__Permissa__3214EC075A5A3F69] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[PermissaoGrupoUsuario]    Script Date: 20/02/2024 08:29:42 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[PermissaoGrupoUsuario](
+	[IdPermissao] [int] NOT NULL,
+	[IdGrupoUsuario] [int] NOT NULL,
+ CONSTRAINT [PK_PermissaoGrupoUsuario] PRIMARY KEY CLUSTERED 
+(
+	[IdPermissao] ASC,
+	[IdGrupoUsuario] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Produto]    Script Date: 20/02/2024 08:29:42 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Produto](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Nome] [varchar](150) NULL,
+	[Descricao] [varchar](50) NULL,
+	[CodigoDeBarra] [varchar](50) NULL,
+	[Preco] [float] NULL,
+	[Quantidade] [int] NULL,
+	[Custo] [float] NULL,
+	[IdMarca] [int] NOT NULL,
+	[IdCategoria] [int] NOT NULL,
+	[IdFornecedor] [int] NULL,
+ CONSTRAINT [PK__Produto__3214EC07F3AC594A] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Senha]    Script Date: 20/02/2024 08:29:42 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Senha](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[IdCliente] [int] NOT NULL,
+ CONSTRAINT [PK_Senha] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Usuario]    Script Date: 20/02/2024 08:29:42 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Usuario](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Nome] [varchar](150) NULL,
+	[NomeUsuario] [varchar](50) NULL,
+	[Email] [varchar](150) NULL,
+	[CPF] [varchar](15) NULL,
+	[Ativo] [bit] NULL,
+	[Senha] [varchar](50) NULL,
+	[DataCadastro] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[UsuarioGrupoUsuario]    Script Date: 20/02/2024 08:29:42 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[UsuarioGrupoUsuario](
+	[IdUsuario] [int] NOT NULL,
+	[IdGrupoUsuario] [int] NOT NULL,
+ CONSTRAINT [PK_UsuarioGrupoUsuario] PRIMARY KEY CLUSTERED 
+(
+	[IdUsuario] ASC,
+	[IdGrupoUsuario] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Venda]    Script Date: 20/02/2024 08:29:42 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Venda](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[IdUsuario] [int] NULL,
+	[IdCliente] [int] NULL,
+	[IdFormaPagamento] [int] NULL,
+	[DataVenda] [datetime] NULL,
+	[Total] [float] NULL,
+ CONSTRAINT [PK_Venda] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Usuario] ADD  DEFAULT (getdate()) FOR [DataCadastro]
+GO
+ALTER TABLE [dbo].[Estoque]  WITH CHECK ADD  CONSTRAINT [FK_Estoque_Produto] FOREIGN KEY([IdProduto])
+REFERENCES [dbo].[Produto] ([Id])
+GO
+ALTER TABLE [dbo].[Estoque] CHECK CONSTRAINT [FK_Estoque_Produto]
+GO
+ALTER TABLE [dbo].[ItemVenda]  WITH CHECK ADD  CONSTRAINT [FK_ItemVenda_Produto1] FOREIGN KEY([IdProduto])
+REFERENCES [dbo].[Produto] ([Id])
+GO
+ALTER TABLE [dbo].[ItemVenda] CHECK CONSTRAINT [FK_ItemVenda_Produto1]
+GO
+ALTER TABLE [dbo].[ItemVenda]  WITH CHECK ADD  CONSTRAINT [FK_ItemVenda_Venda1] FOREIGN KEY([IdVenda])
+REFERENCES [dbo].[Venda] ([Id])
+GO
+ALTER TABLE [dbo].[ItemVenda] CHECK CONSTRAINT [FK_ItemVenda_Venda1]
+GO
+ALTER TABLE [dbo].[Produto]  WITH CHECK ADD  CONSTRAINT [FK_Produto_Categoria] FOREIGN KEY([IdCategoria])
+REFERENCES [dbo].[Categoria] ([Id])
+GO
+ALTER TABLE [dbo].[Produto] CHECK CONSTRAINT [FK_Produto_Categoria]
+GO
+ALTER TABLE [dbo].[Produto]  WITH CHECK ADD  CONSTRAINT [FK_Produto_Fornecedor] FOREIGN KEY([IdFornecedor])
+REFERENCES [dbo].[Fornecedor] ([Id])
+GO
+ALTER TABLE [dbo].[Produto] CHECK CONSTRAINT [FK_Produto_Fornecedor]
+GO
+ALTER TABLE [dbo].[Produto]  WITH CHECK ADD  CONSTRAINT [FK_Produto_Marca] FOREIGN KEY([IdMarca])
+REFERENCES [dbo].[Marca] ([Id])
+GO
+ALTER TABLE [dbo].[Produto] CHECK CONSTRAINT [FK_Produto_Marca]
+GO
+ALTER TABLE [dbo].[Senha]  WITH CHECK ADD  CONSTRAINT [FK_Senha_Cliente] FOREIGN KEY([IdCliente])
+REFERENCES [dbo].[Cliente] ([Id])
+GO
+ALTER TABLE [dbo].[Senha] CHECK CONSTRAINT [FK_Senha_Cliente]
+GO
+ALTER TABLE [dbo].[Venda]  WITH CHECK ADD  CONSTRAINT [FK_Venda_Cliente] FOREIGN KEY([IdCliente])
+REFERENCES [dbo].[Cliente] ([Id])
+GO
+ALTER TABLE [dbo].[Venda] CHECK CONSTRAINT [FK_Venda_Cliente]
+GO
+ALTER TABLE [dbo].[Venda]  WITH CHECK ADD  CONSTRAINT [FK_Venda_FormaPagamento] FOREIGN KEY([IdFormaPagamento])
+REFERENCES [dbo].[FormaPagamento] ([Id])
+GO
+ALTER TABLE [dbo].[Venda] CHECK CONSTRAINT [FK_Venda_FormaPagamento]
+GO
+ALTER TABLE [dbo].[Venda]  WITH CHECK ADD  CONSTRAINT [FK_Venda_Usuario] FOREIGN KEY([IdUsuario])
+REFERENCES [dbo].[Usuario] ([Id])
+GO
+ALTER TABLE [dbo].[Venda] CHECK CONSTRAINT [FK_Venda_Usuario]
+GO
+USE [master]
+GO
+ALTER DATABASE [GestaoMercearia] SET  READ_WRITE 
+GO
 USE GestaoMercearia
 IF(NOT EXISTS(SELECT 1 FROM Permissao WHERE Id = 1))INSERT INTO Permissao(Descricao)VALUES('Visualizar usuário')
 IF(NOT EXISTS(SELECT 1 FROM Permissao WHERE Id = 2))INSERT INTO Permissao(Descricao)VALUES('Cadastrar usuário')
@@ -1102,5 +1438,6 @@ GO
 Insert Into FormaPagamento(Tipo, Descricao, Troco) VALUES ('Dinheiro', 'Uma forma de pagamento física, com cédulas e moedas', 1)
 Insert Into FormaPagamento(Tipo, Descricao, Troco) VALUES ('Cartão de Débito', 'Uma forma de pagamento que usa moeda virtual de conta bancária, de débito instantâneo', 0)
 Insert Into FormaPagamento(Tipo, Descricao, Troco) VALUES ('Pix', 'Uma forma de pagamento eletrônico que permite transferência monetária instantânea', 0)
+
 
 
