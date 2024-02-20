252906e069711118d6213fe392c6602227b65f2f
Keyton Bessa <113356319+KeytonnBessa@users.noreply.github.com>
2024-02-20 08:58:48 -0300

    Botei o campo custo no banco e fiz as mudanças necessarias

--- Banco/Script.sql
+++ Banco/Script.sql
@@ -1431,9 +1431,9 @@ Insert Into Marca(Nome) VALUES ('Nestle')
 Insert Into Marca(Nome) VALUES ('Coca-Cola')
 Insert Into Marca(Nome) VALUES ('Friaca')
 GO
-Insert into Produto(Nome, Descricao, CodigoDeBarra, Preco, Quantidade, IdMarca, IdCategoria, IdFornecedor) Values ('Barra de Cereal', '50g', 'ABCDEF', 6.50, 50, 1, 1, 1)
-Insert into Produto(Nome, Descricao, CodigoDeBarra, Preco, Quantidade, IdMarca, IdCategoria, IdFornecedor) Values ('Refrigerante de Cola', '600ML', 'XXXZZZ', 9.50, 75, 2, 2, 2)
-Insert into Produto(Nome, Descricao, CodigoDeBarra, Preco, Quantidade, IdMarca, IdCategoria, IdFornecedor) Values ('Sorvete', 'Sabor Chocolate', 'AAAEEE', 12.25, 20, 3, 3, 3)
+Insert into Produto(Nome, Descricao, CodigoDeBarra, Preco, Quantidade, IdMarca, IdCategoria, IdFornecedor) Values ('Barra de Cereal', '50g', 'ABCDEF', 6.50, 50, 4.25, 1, 1, 1)
+Insert into Produto(Nome, Descricao, CodigoDeBarra, Preco, Quantidade, IdMarca, IdCategoria, IdFornecedor) Values ('Refrigerante de Cola', '600ML', 'XXXZZZ', 9.50, 75, 7.50, 2, 2, 2)
+Insert into Produto(Nome, Descricao, CodigoDeBarra, Preco, Quantidade, IdMarca, IdCategoria, IdFornecedor) Values ('Sorvete', 'Sabor Chocolate', 'AAAEEE', 12.25, 20, 10, 3, 3, 3)
 GO
 Insert Into FormaPagamento(Tipo, Descricao, Troco) VALUES ('Dinheiro', 'Uma forma de pagamento física, com cédulas e moedas', 1)
 Insert Into FormaPagamento(Tipo, Descricao, Troco) VALUES ('Cartão de Débito', 'Uma forma de pagamento que usa moeda virtual de conta bancária, de débito instantâneo', 0)
